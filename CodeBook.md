ed84359cf890b03ba017b87ba25805a930430bc8
shivanandkoppalkar <shivanand.koppalkar@gmail.com>
2016-03-24 08:45:11 +0000

    CodeBook, ReadMe and run_analysis.R Script

--- CodeBook.md
+++ CodeBook.md
@@ -1,64 +1,56 @@
-# CodeBook
+# Introduction
 
-This is a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data.
+The script `run_analysis.R`
+- downloads the data from
+  [UCI Machine Learning Repository](http://archive.ics.uci.edu/ml/index.html)
+- merges the training and test sets to create one data set
+- replaces `activity` values in the dataset with descriptive activity names
+- extracts only the measurements (features) on the mean and standard deviation
+  for each measurement
+- appropriately labels the columns with descriptive names
+- creates a second, independent tidy dataset with an average of each variable
+  for each each activity and each subject. In other words, same type of
+  measurements for a particular subject and activity are averaged into one value
+  and the tidy data set contains these mean values only. The processed tidy data
+  set is also exported as csv file.
+  
+# run_analysis.R
 
-## The data source
+The script is parititioned into functions such that each function performs one of the
+steps described above. To run whole cleaning procedure, call `clean.data`
+function. The script also assumes that `plyr` library is already installed.
 
-* Original data: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-* Original description of the dataset: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+# The original data set
 
-## Data Set Information
+The original data set is split into training and test sets (70% and 30%,
+respectively) where each partition is also split into three files that contain
+- measurements from the accelerometer and gyroscope
+- activity label
+- identifier of the subject
 
-The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data.
+# Getting and cleaning data
 
-The sensor signals (accelerometer and gyroscope) were pre-processed by applying noise filters and then sampled in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window). The sensor acceleration signal, which has gravitational and body motion components, was separated using a Butterworth low-pass filter into body acceleration and gravity. The gravitational force is assumed to have only low frequency components, therefore a filter with 0.3 Hz cutoff frequency was used. From each window, a vector of features was obtained by calculating variables from the time and frequency domain.
+If the data is not already available in the `data` directory, it is downloaded
+from UCI repository.
 
-## The data
+The first step of the preprocessing is to merge the training and test
+sets. Two sets combined, there are 10,299 instances where each
+instance contains 561 features (560 measurements and subject identifier). After
+the merge operation the resulting data, the table contains 562 columns (560
+measurements, subject identifier and activity label).
 
-The dataset includes the following files:
+After the merge operation, mean and standard deviation features are extracted
+for further processing. Out of 560 measurement features, 33 mean and 33 standard
+deviations features are extracted, yielding a data frame with 68 features
+(additional two features are subject identifier and activity label).
 
-- 'README.txt'
+Next, the activity labels are replaced with descriptive activity names, defined
+in `activity_labels.txt` in the original data folder.
 
-- 'features_info.txt': Shows information about the variables used on the feature vector.
+The final step creates a tidy data set with the average of each variable for
+each activity and each subject. 10299 instances are split into 180 groups (30
+subjects and 6 activities) and 66 mean and standard deviation features are
+averaged for each group. The resulting data table has 180 rows and 66 columns.
+The tidy data set is exported to `UCI_HAR_tidy.csv` where the first row is the
+header containing the names for each column.
 
-- 'features.txt': List of all features.
-
-- 'activity_labels.txt': Links the class labels with their activity name.
-
-- 'train/X_train.txt': Training set.
-
-- 'train/y_train.txt': Training labels.
-
-- 'test/X_test.txt': Test set.
-
-- 'test/y_test.txt': Test labels.
-
-The following files are available for the train and test data. Their descriptions are equivalent.
-
-- 'train/subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30.
-
-- 'train/Inertial Signals/total_acc_x_train.txt': The acceleration signal from the smartphone accelerometer X axis in standard gravity units 'g'. Every row shows a 128 element vector. The same description applies for the 'total_acc_x_train.txt' and 'total_acc_z_train.txt' files for the Y and Z axis.
-
-- 'train/Inertial Signals/body_acc_x_train.txt': The body acceleration signal obtained by subtracting the gravity from the total acceleration.
-
-- 'train/Inertial Signals/body_gyro_x_train.txt': The angular velocity vector measured by the gyroscope for each window sample. The units are radians/second.
-
-
-## Transformation details
-
-There are 5 parts:
-
-1. Merges the training and the test sets to create one data set.
-2. Extracts only the measurements on the mean and standard deviation for each measurement.
-3. Uses descriptive activity names to name the activities in the data set
-4. Appropriately labels the data set with descriptive activity names.
-5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-## How ```run_analysis.R``` implements the above steps:
-
-* Require ```reshapre2``` and ```data.table``` librareis.
-* Load both test and train data
-* Load the features and activity labels.
-* Extract the mean and standard deviation column names and data.
-* Process the data. There are two parts processing test and train data respectively.
-* Merge data set.
--- run_analysis.R
+++ run_analysis.R
@@ -1,190 +1,106 @@
-# Program Name: run_analysis.R
-# March 22, 2016
+# This R script gets and performs some cleaning on human activity data, built
+# from recordings of subjects performing daily activities while carrying
+# smartphone. The full description of the data set is available at:
+# http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
-library(dplyr)
+library(plyr)
 
-# set working directory
-setwd("C:/Work")
+download.data = function() {
+    "Checks for data directory and creates one if it doesn't exist"
+    if (!file.exists("data")) {
+        message("Creating data directory")
+        dir.create("data")
+    }
+    if (!file.exists("data/UCI HAR Dataset")) {
+        # download the data
+        fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        zipfile="data/UCI_HAR_data.zip"
+        message("Downloading data")
+        download.file(fileURL, destfile=zipfile, method="curl")
+        unzip(zipfile, exdir="data")
+    }
+}
 
-# Read features.txt and capture the complete list of variables
-# of each vector
-# Column V1: Count
-# Column V2: Feature
-# 561 entries: These are the column names of the text data files.
-features<-read.table("features.txt")
-class(features)
+merge.datasets = function() {
+    "Merge training and test datasets"
+    # Read data
+    message("reading X_train.txt")
+    training.x <- read.table("data/UCI HAR Dataset/train/X_train.txt")
+    message("reading y_train.txt")
+    training.y <- read.table("data/UCI HAR Dataset/train/y_train.txt")
+    message("reading subject_train.txt")
+    training.subject <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
+    message("reading X_test.txt")
+    test.x <- read.table("data/UCI HAR Dataset/test/X_test.txt")
+    message("reading y_test.txt")
+    test.y <- read.table("data/UCI HAR Dataset/test/y_test.txt")
+    message("reading subject_test.txt")
+    test.subject <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
+    # Merge
+    merged.x <- rbind(training.x, test.x)
+    merged.y <- rbind(training.y, test.y)
+    merged.subject <- rbind(training.subject, test.subject)
+    # merge train and test datasets and return
+    list(x=merged.x, y=merged.y, subject=merged.subject)
+}
 
-# Read the data files and capture the information
-# in a R data frame:
-# act_labels: activity_labels.txt (Values are from 1 to 6, indicates activity name)
-# y_train: y_train.txt (Values are 1 to 6 = activity labels, 7,352 entries)
-# y_test: y_test.txt (Values are 1 to 6 = activity labels, 2947 entries)
+extract.mean.and.std = function(df) {
+    # Given the dataset (x values), extract only the measurements on the mean
+    # and standard deviation for each measurement.
 
-act_labels<-read.table("activity_labels.txt")
-y_train<-read.table("y_train.txt")
-y_test<-read.table("y_test.txt")
+    # Read the feature list file
+    features <- read.table("data/UCI HAR Dataset/features.txt")
+    # Find the mean and std columns
+    mean.col <- sapply(features[,2], function(x) grepl("mean()", x, fixed=T))
+    std.col <- sapply(features[,2], function(x) grepl("std()", x, fixed=T))
+    # Extract them from the data
+    edf <- df[, (mean.col | std.col)]
+    colnames(edf) <- features[(mean.col | std.col), 2]
+    edf
+}
 
-### TRAIN GROUP Information data ###
-# Number of records and columns checked using dim.data.frame() 
-# Range of values for subject_* checked using unique() 
-# head() function used for each TR_* variables to check if the way read.table() captured
-# the information from the text data files makes sense
+name.activities = function(df) {
+    # Use descriptive activity names to name the activities in the dataset
+    colnames(df) <- "activity"
+    df$activity[df$activity == 1] = "WALKING"
+    df$activity[df$activity == 2] = "WALKING_UPSTAIRS"
+    df$activity[df$activity == 3] = "WALKING_DOWNSTAIRS"
+    df$activity[df$activity == 4] = "SITTING"
+    df$activity[df$activity == 5] = "STANDING"
+    df$activity[df$activity == 6] = "LAYING"
+    df
+}
 
-# subject_train: subject_train.txt: identifies the subject who performed the activity, 7,352 entries
-#                values are 1, 3, 5, 6, 7, 8, 11, 14, 15, 16, 17, 19, 21, 22, 23 25, 26, 27, 28, 29, 30
-#
-# TR_body_acc_x: body_acc_x_train.txt; 7352R x 128C records
-#                body acceleration signal from smartphone X-axis
-#                obtained by subtracting the gravity from total acceleration 
-#                7352R x 128C records
-# TR_body_acc_y: body_acc_y_train.txt; 7352R x 128C records
-#                body acceleration signal from smartphone Y-axis 
-#                obtained by subtracting the gravity from total acceleration
-#                7352R x 128C records
-# TR_body_acc_z: body_acc_z_train.txt; 7352R x 128C records
-#                body acceleration signal from smartphone Z-axis 
-#                obtained by subtracting the gravity from total acceleration 
-#                7352R x 128C records
+bind.data <- function(x, y, subjects) {
+    # Combine mean-std values (x), activities (y) and subjects into one data
+    # frame.
+    cbind(x, y, subjects)
+}
 
-# TR_body_gyro_x: body_gyro_x_train.txt; 7352R x 128C records
-#                 angular velocity vector measured by gyroscope for each sample 
-#                 in radians/second for the X-axis   
-# TR_body_gyro_y: body_gyro_y_train.txt; 7352R x 128C records
-#                 angular velocity vector measured by gyroscope for each sample in radians/second   
-#                 in radians/second for the X-axis
-# TR_body_gyro_z: body_gyro_z_train.txt; 7352R x 128C records
-#                 angular velocity vector measured by gyroscope for each sample in radians/second   
-#                 in radians/second for the X-axis
+create.tidy.dataset = function(df) {
+    # Given X values, y values and subjects, create an independent tidy dataset
+    # with the average of each variable for each activity and each subject.
+    tidy <- ddply(df, .(subject, activity), function(x) colMeans(x[,1:60]))
+    tidy
+}
 
-# TR_tot_acc_x: total_acc_x_train.txt; 7352R x 128C records
-#               acceleration signal from smartphone accelerometer X axis in std gravity units 
-# TR_tot_acc_y: total_acc_y_train.txt; 7352R x 128C records
-#               acceleration signal from smartphone accelerometer Y axis in std gravity units 
-# TR_tot_acc_z: total_acc_z_train.txt; 7352R x 128C records
-#               acceleration signal from smartphone accelerometer Z axis in std gravity units 
-# Only the subject_train.txt file was directly used to produce desired output file
-# Other files were read and stored to data frames to check contents and see if there are information
-# needed for producing output file which are not availble from the X_train.txt and X_test.txt files.
-
-subject_train<-read.table("subject_train.txt")
-
-### TEST GROUP Information data ###
-# Number of records and columns checked using dim.data.frame() to check for functionality
-# Range of values for subject_* checked using unique() 
-# head() function used for each TEST_* variables to check if the way read.table() captured
-# the information from the text data files makes sense
-
-# subject_test: subject_test.txt: identifies the subject who performed the activity, 2,947 entries
-#                values are 2, 4, 9, 10, 12, 13, 18, 20, 24
-#
-# TEST_body_acc_x: body_acc_x_test.txt; 2947R x 128C records
-#                body acceleration signal from smartphone X-axis
-#                obtained by subtracting the gravity from total acceleration 
-#                7352R x 128C records
-# TEST_body_acc_y: body_acc_y_test.txt; 2947R x 128C records
-#                body acceleration signal from smartphone Y-axis 
-#                obtained by subtracting the gravity from total acceleration
-#                7352R x 128C records
-# TEST_body_acc_z: body_acc_z_test.txt; 2947R x 128C records
-#                body acceleration signal from smartphone Z-axis 
-#                obtained by subtracting the gravity from total acceleration 
-#                7352R x 128C records
-
-# TEST_body_gyro_x: body_gyro_x_test.txt; 2947R x 128C records
-#                 angular velocity vector measured by gyroscope for each sample 
-#                 in radians/second for the X-axis   
-# TEST_body_gyro_y: body_gyro_y_test.txt; 2947R x 128C records
-#                 angular velocity vector measured by gyroscope for each sample in radians/second   
-#                 in radians/second for the X-axis
-# TEST_body_gyro_z: body_gyro_z_test.txt; 2947R x 128C records
-#                 angular velocity vector measured by gyroscope for each sample in radians/second   
-#                 in radians/second for the X-axis
-
-# TEST_tot_acc_x: total_acc_x_train.txt; 2947R x 128C records
-#               acceleration signal from smartphone accelerometer X axis in std gravity units 
-# TEST_tot_acc_y: total_acc_y_train.txt; 2947R x 128C records
-#               acceleration signal from smartphone accelerometer Y axis in std gravity units 
-# TEST_tot_acc_z: total_acc_z_train.txt; 2947R x 128C records
-#               acceleration signal from smartphone accelerometer Z axis in std gravity units 
-# Only the subject_test.txt file was directly used to produce desired output file
-# Other files were read and stored to data frames to check contents and see if there are information
-# needed for producing output file which are not availble from the X_train.txt and X_test.txt files.
-
-subject_test<-read.table("subject_test.txt")
-
-#####################################################################
-#1. Merge the training and the test sets to create one data set.
-#2. Extract only measurements on the mean and standard deviation
-#for each measurement. 
-#4. Label the data set with descriptive variable names
-#####################################################################
-
-# Read the data files and capture the information
-# in a R dataframe:
-# x_train: X_train.txt (7352R x 561C entries on Training information)
-# x_test : X_test.txt (2947R x 561C entries on Test information)
-# Use features dataframe to specify the column names 
-# for both x_train and x_test(#4)
-
-x_train<-read.table("X_train.txt",col.names=features$V2)
-x_test<-read.table("X_test.txt",col.names=features$V2)
-
-# Append values of subject_train and subject_test (subject identifier) to
-# x_train and x_test, respectively
-
-x_train<-cbind(x_train,subject_train)
-x_test<-cbind(x_test,subject_test)
-
-# Append values of y_train and y_test (activity identifier) to
-# x_train and x_test, respectively
-
-x_train<-cbind(x_train,y_train)
-x_test<-cbind(x_test,y_test)
-
-# Rename appended column to Subject_id and activity
-
-names(x_train)[562]<-"subject_id"
-names(x_test)[562]<-"subject_id"
-
-names(x_train)[563]<-"activity"
-names(x_test)[563]<-"activity"
-
-# capture column numbers to include in the merged data
-# Sub-features only include mean(), std(), subject_id and activity (#2)
-
-SubFeatures<-c(grep(pattern=".*-(mean|std)\\(\\)",features$V2,value=FALSE),562,563)
-x_train<-x_train[,SubFeatures]
-x_test<-x_test[,SubFeatures]
-
-# add column to indicate if subject is from Train or Test groups
-x_train<-mutate(x_train,subject_grp="train")
-x_test<-mutate(x_test,subject_grp="test")
-
-# Merge the train and test data to one data frame.(#1)
-AllData<-rbind(x_train,x_test)
-
-#####################################################################
-#3. Use descriptive activity names to name activities in the dataset
-#####################################################################
-AllData<-merge(AllData,act_labels,by.x='activity', by.y='V1')
-names(AllData)[70]<-"desc_activity"
-
-##############################################################################
-# #5 creates a second, independent tidy data set with the average 
-# of each variable for each activity and each subject after (1) to (4).
-# Thus, the mean will be calculated from Columns 2 to 67.
-###############################################################################
-
-TidySet<-aggregate(AllData[, 2:67], list(AllData$subject_id,AllData$desc_activity), mean)
-TidySet<-transform(TidySet,Group.1=as.character(Group.1))
-
-# To export TidySet data frame to a text file, install the
-# MASS package: install.packages("MASS")
-# and then load the library via library(MASS) command
-# Output file is Output_Mean_Grouped.txt
-
-library(MASS)
-write.matrix(TidySet,file="tidy_data.txt",sep=" ")
-
-# use rm(list=ls()) to remove variables from workspace when 
-# done using the data frames
+clean.data = function() {
+    # Download data
+    download.data()
+    # merge training and test datasets. merge.datasets function returns a list
+    # of three dataframes: X, y, and subject
+    merged <- merge.datasets()
+    # Extract only the measurements of the mean and standard deviation for each
+    # measurement
+    cx <- extract.mean.and.std(merged$x)
+    # Name activities
+    cy <- name.activities(merged$y)
+    # Use descriptive column name for subjects
+    colnames(merged$subject) <- c("subject")
+    # Combine data frames into one
+    combined <- bind.data(cx, cy, merged$subject)
+    # Create tidy dataset
+    tidy <- create.tidy.dataset(combined)
+    # Write tidy dataset as csv
+    write.csv(tidy, "UCI_HAR_tidy.csv", row.names=FALSE)
+}
